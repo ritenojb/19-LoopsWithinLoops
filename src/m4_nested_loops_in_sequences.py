6c10e49e1bc5ccf4d96409a780f76b058a1b5e4a
ritenojb <ritenojb@rose-hulman.edu>
2018-10-18 16:11:52 -0400

    DONE

--- src/m4_nested_loops_in_sequences.py
+++ src/m4_nested_loops_in_sequences.py
@@ -284,7 +284,7 @@ def print_characters_slanted(sequence_of_strings):
     Precondition:  the given argument is a sequence of strings.
     """
     # ------------------------------------------------------------------
-    # DONE: 6. Implement and test this function.
+    # DONE : 6. Implement and test this function.
     #  ** READ THE TESTS that have been written for you (ABOVE).
     #  ** ASK QUESTIONS if you do not understand the TESTS (ABOVE).
     #
